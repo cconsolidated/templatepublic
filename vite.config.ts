c76dae2882211de80c4fad745b383f8aa7c6977b
4M4Z3 <>
2025-06-21 18:26:17 -0500

    Push
    New version using skypack

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'build/client',
-    emptyOutDir: true,
-  },
+    emptyOutDir: true
+  }
 }); 
